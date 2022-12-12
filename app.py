60656dd44c5c769b3ff49119615d9529c89c3083
Fabian-2596 <fabianschmidt9625@gmx.de>
2022-12-12 13:34:42 +0000

    added ip

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ CORS(app)
 # My SQL Instance configurations
 # Change these details to match your instance configurations
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'a'
 app.config['MYSQL_DB'] = 'student'
-app.config['MYSQL_HOST'] = '35.205.42.222'
+app.config['MYSQL_HOST'] = '34.67.193.225'
 mysql.init_app(app)
 
 
